f90b9d4f43726c192914e87472036876981edb1d
Kacper Radota <72502652+KacperRadota@users.noreply.github.com>
2024-04-18 20:01:30 +0200

    Changes

--- main.tf
+++ main.tf
@@ -3,19 +3,54 @@ provider "aws" {
 }
 
 resource "aws_vpc" "mainvpc" {
-	cidr_block = "10.1.0.0/16"
+	cidr_block = "10.0.0.0/16"
+	enable_dns_support = true
+	enable_dns_hostnames = true
 }
 
 resource "aws_subnet" "main_subnet" {
     vpc_id     = aws_vpc.mainvpc.id
-    cidr_block = "10.1.0.0/16"
+    cidr_block = "10.0.1.0/24"
     availability_zone = "us-east-1a"
+}
+
+resource "aws_internet_gateway" "maingateway" {
+	vpc_id = aws_vpc.mainvpc.id
+}
+
+resource "aws_route_table" "mainroutetable" {
+	vpc_id = aws_vpc.mainvpc.id
+	
+	route {
+		cidr_block = "0.0.0.0/0"
+		gateway_id = aws_internet_gateway.maingateway.id
+	}
+}
+
+resource "aws_route_table_association" "main_subnet" {
+	subnet_id = aws_subnet.main_subnet.id
+	route_table_id = aws_route_table.mainroutetable.id
 }
 
 resource "aws_security_group" "app_sg" {
     name        = "app-security-group"
     description = "Security group for the application"
 	vpc_id = aws_vpc.mainvpc.id
+	
+	  egress {
+		from_port   = 0
+		to_port     = 0
+		protocol    = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+	  }
+
+	  ingress {
+		from_port   = 22
+		to_port     = 22
+		protocol    = "tcp"
+		description = "SSH"
+		cidr_blocks = ["0.0.0.0/0"]
+	  }
 
     ingress {
       from_port   = 3000
@@ -35,41 +70,20 @@ resource "aws_security_group" "app_sg" {
 resource "aws_instance" "server" {
 	ami = "ami-051f8a213df8bc089"
 	instance_type = "t2.micro"
+	associate_public_ip_address = true
+	key_name = "default"
 	
 	tags = {
-		Name = "Server"
+		Name = "Server"	
 	}
 	
-	user_data = <<-EOF
-    #!/bin/bash
-    sudo yum update -y
-    sudo amazon-linux-extras install docker -y
-    sudo service docker start
-    sudo usermod -a -G docker ec2-user
-
-    # Install Docker Compose
-    sudo curl -L "https://github.com/docker/compose/releases/download/{VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
-    sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-	EOF
+	user_data = "${file("install.sh")}"
 	
 	connection {
 		type = "ssh"
 		user = "ec2-user"
 		private_key = file("C:/Users/kacpe/.awsLabKeys/default.pem")
 		host = aws_instance.server.public_ip
-	}
-	
-	provisioner "file" {
-		source = "docker-compose.yml"
-		destination = "/home/ec2-user/docker-compose.yml"
-	}
-	
-	provisioner "remote-exec" {
-		inline = [
-			"cd /home/ec2-user",
-			"docker-compose up -d"
-		]
 	}
 		
 	subnet_id = aws_subnet.main_subnet.id
